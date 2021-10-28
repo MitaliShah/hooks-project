fdb69d8f006742e59dbf4240bc7f2f25ff805367
mitali shah <mitalishaleenshah@gmail.com>
2021-10-28 16:04:26 -0500

    added useContext component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+
+// components
+// import UseMemoExample from './UseMemoExample';
+import UseContextExample from './components/UseContextExample';
+// import UseCallbackExample from './UseCallbackExample';
+// import UseRefExample from './UseRefExample';
+
+// styles
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+    <>
+        <UseContextExample />
+        {/* <UseRefExample /> */}
+        {/* <UseMemoExample /> */}        
+        {/* <UseCallbackExample />*/}
+    </>
+);
 
 export default App;
